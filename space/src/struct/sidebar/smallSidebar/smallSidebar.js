2a8251f2cf84e0ecc831f60a6230b5a187330c92
singh943 <singhvir@berkeley.edu>
2025-04-22 22:20:09 -0700

    Settings Button in Sidebar

--- space/src/struct/sidebar/smallSidebar/smallSidebar.js
+++ space/src/struct/sidebar/smallSidebar/smallSidebar.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import './smallSidebar.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faEdit, faPlus, faCalendar, faFolder, faCog, faStream, faLayerGroup, faSearch, faDashboard, faDatabase, faChartBar } from '@fortawesome/free-solid-svg-icons';
+import { faEdit, faPlus, faCalendar, faFolder, faCog, faStream, faLayerGroup, faSearch, faDashboard, faDatabase, faChartBar, faGear } from '@fortawesome/free-solid-svg-icons';
 import { ChatBubbleLeftRightIcon } from '@heroicons/react/24/solid';
 import SearchSidebar from '../searchSidebar/searchSidebar';
 import { useCreatePostContext } from '../../../context/CreatePostContext';
@@ -10,13 +10,16 @@ import { useCreatePostContext } from '../../../context/CreatePostContext';
 const SmallSidebar = ({ setSearchSidebarOpen, searchSidebarOpen }) => {
     const { openCreatePostOverlay } = useCreatePostContext();
 
-    const taskFlow = [
+    const topIcons = [
         { icon: <FontAwesomeIcon icon={faChartBar} />, label: 'Dasboard', path: '/dashboard', type: 'link' },
         { icon: <FontAwesomeIcon icon={faStream} />, label: 'Timeline', path: '/timeline', type: 'link' },
         { icon: <FontAwesomeIcon icon={faLayerGroup} />, label: 'Explore', path: '/explore', type: 'link' },
         { icon: <FontAwesomeIcon icon={faSearch} />, label: 'Search', onClick: () => { searchSidebarOpen ? setSearchSidebarOpen(false) : setSearchSidebarOpen(true) }, type: 'button' },
         { icon: <ChatBubbleLeftRightIcon className='chat-icon' />, label: 'Messages', path: '/messages', type: 'link' },
         { icon: <FontAwesomeIcon icon={faEdit} />, label: 'Create Post', onClick: () => openCreatePostOverlay(window.location.pathname), type: 'button' },
+    ];
+    const bottomIcons = [
+        { icon: <FontAwesomeIcon icon={faGear} />, label: 'Settings', path: '/settings', type: 'link' },
     ];
 
     const navigate = useNavigate();
@@ -33,7 +36,8 @@ const SmallSidebar = ({ setSearchSidebarOpen, searchSidebarOpen }) => {
 
     return (
         <div className={`small-sidebar ${searchSidebarOpen ? 'search-sidebar-open' : ''}`}>
-            {taskFlow?.map((item, index) => (
+            <div className="small-sidebar-top">
+            {topIcons?.map((item, index) => (
                 <div
                     key={index}
                     className="small-sidebar-item"
@@ -47,6 +51,23 @@ const SmallSidebar = ({ setSearchSidebarOpen, searchSidebarOpen }) => {
                     <div className="tooltip">{item.label}</div>
                 </div>
             ))}
+                </div>
+            <div className="small-sidebar-bottom">
+            {bottomIcons?.map((item, index) => (
+                <div
+                    key={index}
+                    className="small-sidebar-item"
+                    onClick={(e) => { handleClick(item); e.stopPropagation(); }}
+                >
+                    {item.type === 'button' ? (
+                        <button>{item.icon}</button>
+                    ) : (
+                        <Link to={item.path} onClick={(e) => e.stopPropagation()}>{item.icon}</Link>
+                    )}
+                    <div className="tooltip">{item.label}</div>
+                </div>
+            ))}
+            </div>
             {<SearchSidebar isOpen={searchSidebarOpen} />}
         </div>
     );
