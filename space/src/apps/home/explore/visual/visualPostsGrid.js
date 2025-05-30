62d9d599898e4cef08c8f3ff1d93d7b8a4c8636b
singh943 <singhvir@berkeley.edu>
2025-05-29 21:44:48 -0700

    Explore Page - Upgrade - Prior

--- space/src/apps/home/explore/visual/visualPostsGrid.js
+++ space/src/apps/home/explore/visual/visualPostsGrid.js
@@ -6,7 +6,6 @@ import { usePostContext } from '../../../../context/PostContext';
 const VisualPostsGrid = ({ posts }) => {
   const { handleExpandPostOpen } = usePostContext();
 
-  console.log('VisualPostsGrid posts:', posts);
   return (
     <div className="visual-posts-grid">
       <PostsGrid classname={'explore'} postsData={posts} handleExpandPostOpen={handleExpandPostOpen} />
