787b970462a4632f5703456b2e38b434737ccd57
singh943 <singhvir@berkeley.edu>
2025-06-05 20:19:09 -0700

    Profile - My Posts + My Communities - Intact

--- api/src/user/data/urls.py
+++ api/src/user/data/urls.py
@@ -2,5 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # Posts:
     path('posts/<str:username>/list/', views.get_profile_posts_list, name='posts-list'),
+    # Exchanges:
+    path('<str:username>/exchanges/', views.get_user_exchanges, name='get_user_exchanges'),
+    # Communities the user created:
+    path('<str:username>/communities/', views.get_user_communities, name='get_user_communities'),
+
 ]
