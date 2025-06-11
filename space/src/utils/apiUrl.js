469993e19a03fc5a57e38b0fb6d6523e24b7111c
singh943 <singhvir@berkeley.edu>
2025-06-10 21:52:33 -0700

    fix url deploy

--- space/src/utils/apiUrl.js
+++ space/src/utils/apiUrl.js
@@ -1,9 +1,9 @@
 // utils/config/config.js
 
 const API_BASE_URL =
-  process.env.NODE_ENV === 'production'
-    ? process.env.REACT_APP_API_BASE_URL
-    : 'http://localhost:8000/'; // fallback for local dev
+//   process.env.NODE_ENV === 'production'
+     process.env.REACT_APP_API_BASE_URL
+   //  : 'http://localhost:8000/'; // fallback for local dev
 
 export const CLIENT_BASE_URL =
   process.env.REACT_APP_CLIENT_BASE_URL || 'http://localhost:3000';
