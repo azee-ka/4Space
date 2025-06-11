207da9503fd48a9b39e4450176f65e99094f97d3
singh943 <singhvir@berkeley.edu>
2025-06-10 21:23:13 -0700

    Clean up - try deply

--- space/src/utils/apiUrl.js
+++ space/src/utils/apiUrl.js
@@ -1,9 +1,14 @@
 // utils/config/config.js
-const API_BASE_URL = 'http://10.0.0.6:8000/';
- // process.env.NODE_ENV === 'production'
-  //  ? 'https://django-backend-url' : 
-   // process.env.REACT_APP_API_BASE_URL// || 'http://127.0.0.1:8000/';
-export const CLIENT_BASE_URL = 'http://10.0.0.85:3000';
-export const WS_BASE_URL = process.env.REACT_APP_WS_BASE_URL || 'ws://localhost:8000';
+
+const API_BASE_URL =
+  process.env.NODE_ENV === 'production'
+    ? process.env.REACT_APP_API_BASE_URL
+    : 'http://localhost:8000/'; // fallback for local dev
+
+export const CLIENT_BASE_URL =
+  process.env.REACT_APP_CLIENT_BASE_URL || 'http://localhost:3000';
+
+export const WS_BASE_URL =
+  process.env.REACT_APP_WS_BASE_URL || 'ws://localhost:8000';
 
 export default API_BASE_URL;
