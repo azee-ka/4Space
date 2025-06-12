f47ca84ae58b3b7fe8ce5db1165e5c81af2a9918
azee-ka <singhvir943@gmail.com>
2025-06-12 13:27:58 -0700

    upgrading to modb

--- space/src/pages/auth/third_party_buttons/AppleSignInButton.js
+++ space/src/pages/auth/third_party_buttons/AppleSignInButton.js
@@ -4,20 +4,20 @@ import './apple-button.css';
 const AppleSignInButton = () => {
   useEffect(() => {
     const loadAppleSDK = () => {
-        try {
-      if (!document.getElementById('apple-signin-script')) {
-        const script = document.createElement('script');
-        script.id = 'apple-signin-script';
-        script.src = 'https://appleid.cdn-apple.com/appleauth/static/jsapi/appleid/1/en_US/appleid.auth.js';
-        script.async = true;
-        script.onload = initApple;
-        document.body.appendChild(script);
-      } else {
-        initApple();
-      }
-    } catch (err) {
+      try {
+        if (!document.getElementById('apple-signin-script')) {
+          const script = document.createElement('script');
+          script.id = 'apple-signin-script';
+          script.src = 'https://appleid.cdn-apple.com/appleauth/static/jsapi/appleid/1/en_US/appleid.auth.js';
+          script.async = true;
+          script.onload = initApple;
+          document.body.appendChild(script);
+        } else {
+          initApple();
+        }
+      } catch (err) {
         console.error('Error rendering apple btn', err);
-    }
+      }
     };
 
     const initApple = () => {
@@ -35,20 +35,22 @@ const AppleSignInButton = () => {
   }, []);
 
   return (
-    <div className="apple-signin-wrapper">
-      <div
+<div className="apple-signin-wrapper">
+  <div
   id="appleid-signin"
   data-mode="center-align"
-  data-type="continue"
+  data-type="sign-in"
   data-color="black"
   data-border="false"
   data-border-radius="50"
+  data-width="304"
   data-height="40"
   data-logo-size="large"
-  data-logo-position="3"
+  data-logo-position="11"
   data-label-position="88"
 ></div>
-    </div>
+</div>
+
   );
 };
 
